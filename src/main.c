0fe2658448aa705b774efe75414bf1ecb5c07287
bumbread <thebumboni@gmail.com>
2021-01-05 04:25:23 +1100

    improved parser / error checking

--- src/main.c
+++ src/main.c
@@ -28,9 +28,18 @@ u64      typedef ptr;
 void panicf(char *message, ...) {
   va_list args;
   va_start(args, message);
-  vprintf("%s\n", message, args);
+  vprintf(message, args);
   va_end(args);
   exit(1);
+}
+
+static char const *last_error;
+void set_error(char const *message) {
+  last_error = message;
+}
+
+void reset_error(void) {
+  last_error = null;
 }
 
 enum {
@@ -69,41 +78,45 @@ static void state_init(t_lexstate *state, char const *stream) {
 
 static void state_parse_next_token(t_lexstate *state) {
   char const *start = state->stream;
-  if(isalpha(*state->stream) || *state->stream == '_') {
-    state->last_token.kind = TOKEN_IDN;
-    while(isalnum(*state->stream) || *state->stream == '_') {
+  
+  while(true) {
+    if(isalpha(*state->stream) || *state->stream == '_') {
+      state->last_token.kind = TOKEN_IDN;
+      while(isalnum(*state->stream) || *state->stream == '_') {
+        state->stream += 1;
+        state->offset += 1;
+      }
+    }
+    else if(isdigit(*state->stream)) {
+      u64 value = 0;
+      state->last_token.kind = TOKEN_INT;
+      while(isdigit(*state->stream)) {
+        value *= 10;
+        value += *state->stream - '0';
+        state->stream += 1;
+        state->offset += 1;
+      }
+      state->last_token.int_value = value;
+    }
+    else if(isspace(*state->stream)) {
       state->stream += 1;
-      state->offset += 1;
+      if(*state->stream == '\n') {
+        state->offset = 0;
+        state->line += 1;
+      }
+      continue;
     }
-  }
-  else if(isdigit(*state->stream)) {
-    u64 value = 0;
-    state->last_token.kind = TOKEN_INT;
-    while(isdigit(*state->stream)) {
-      value *= 10;
-      value += *state->stream - '0';
+    else {
+      state->last_token.kind = *state->stream;
+      state->offset += 1;
       state->stream += 1;
-      state->offset += 1;
     }
-    state->last_token.int_value = value;
+    char const *end = state->stream;
+    state->last_token.start = start;
+    state->last_token.end = end;
+    break;
   }
-  else if(isspace(*state->stream)) {
-    state->stream += 1;
-    if(*state->stream == '\n') {
-      state->offset = 0;
-      state->line += 1;
-    }
-  }
-  else {
-    state->last_token.kind = *state->stream;
-    state->offset += 1;
-    state->stream += 1;
-  }
-  char const *end = state->stream;
-  state->last_token.start = start;
-  state->last_token.end = end;
 }
-
 static inline bool token_is(t_lexstate *state, t_token_kind kind) {
   return state->last_token.kind == kind;
 }
@@ -119,28 +132,28 @@ static inline bool token_match(t_lexstate *state, t_token_kind kind) {
 // what about --3?
 
 //
-// expr3 = val | '(' val ')'
+// expr3 = val | '(' expr0 ')'
 // expr2 = expr3 | -expr3
 // expr1 = expr2 [('*'|'/') expr2 ...]
 // expr0 = expr1 [('+'|'-') expr1 ...]
 // expr  = expr2
 //
 
-static i64 parse_expr(t_lexstate *state);
+static i64 parse_expr0(t_lexstate *state);
 
 static i64 parse_expr3(t_lexstate *state) {
   if(token_match(state, TOKEN_INT)) {
     return state->last_token.int_value;
   }
   else if(token_match(state, '(')) {
-    i64 res = parse_expr(state);
+    i64 res = parse_expr0(state);
     if(false == token_match(state, ')')) {
-      panicf("fatal: unmatched parenthesis");
+      set_error("fatal: unmatched parenthesis");
     }
     return res;
   }
   else {
-    panicf("fatal: unexpected token");
+    set_error("fatal: unexpected token");
   }
   return 0;
 }
@@ -159,7 +172,7 @@ static i64 parse_expr1(t_lexstate *state) {
     if(op == '*') val *= rval;
     if(op == '/') {
       if (rval == 0) {
-        panicf("error division by zero");
+        set_error("error division by zero");
       }
       val /= rval;
     }
@@ -180,7 +193,11 @@ static i64 parse_expr0(t_lexstate *state) {
 }
 
 static i64 parse_expr(t_lexstate *state) {
-  return parse_expr0(state);
+  i64 result = parse_expr0(state);
+  if(state->last_token.kind != TOKEN_EOF) {
+    set_error("not an expression!\n");
+  }
+  return result;
 }
 
 static void token_print(t_token token) {
@@ -206,7 +223,6 @@ static u64 test_parse_expression(char const *expression) {
   state_init(&state, expression);
   state_parse_next_token(&state);
   u64 result = parse_expr(&state);
-  printf("%d\n", result);
   return result;
 }
 
@@ -217,6 +233,8 @@ static void test_parsing(void) {
   test(-1);
   test(2+(2*2));
   test(2*-2+2);
+  test_parse_expression("2a");
+  test_parse_expression("a");
 }
 #undef test
 
@@ -236,6 +254,20 @@ static void test_lexing(void) {
 int main(void) {
   test_lexing();
   test_parsing();
+  
+  last_error = null;
+  while(true) {
+    char buf[256];
+    printf("> "); fgets(buf, sizeof buf, stdin);
+    u64 result = test_parse_expression(buf);
+    if(last_error == null) {
+      printf("%lld\n", result);
+    }
+    else {
+      printf("error: %s\n", last_error);
+      last_error = null;
+    }
+  }
   
   return 0;
 }
