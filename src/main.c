f5ec953032333a70bfc40432fbe7cf2c5d9e9f4e
bumbread <yyakut.ac@gmail.com>
2021-02-14 02:44:29 +1100

    compiled on linux

--- src/main.c
+++ src/main.c
@@ -18,31 +18,31 @@
 #include"test.c"
 
 int main(void) {
-  test_lexing();
-  test_interns();
-  
-  init_interns(malloc);
-  parser_init_memory(10*mb, malloc(10*mb));
-  
-  byte *buf;
-  char const *filename = "test.x";
-  FILE *input = fopen(filename, "rb");
-  if(null == input) {
-    printf("filename '%s' not found\n", filename);
-  }
-  
-  fseek(input, 0, SEEK_END);
-  size_t input_size = ftell(input);
-  fseek(input, 0, SEEK_SET);
-  buf = malloc(input_size);
-  fread(buf, input_size, 1, input);
-  
-  t_lexstate state;
-  lex_init(&state, buf);
-  lex_next_token(&state);
-  t_ast_node *code = parse_stmts(&state);
-  check_errors();
-  ast_node_print_lisp(code, 0);
-  
-  return 0;
+    test_lexing();
+    test_interns();
+    
+    init_interns(malloc);
+    parser_init_memory(10*mb, malloc(10*mb));
+    
+    byte *buf;
+    char const *filename = "test.x";
+    FILE *input = fopen(filename, "rb");
+    if(null == input) {
+        printf("filename '%s' not found\n", filename);
+    }
+    
+    fseek(input, 0, SEEK_END);
+    size_t input_size = ftell(input);
+    fseek(input, 0, SEEK_SET);
+    buf = malloc(input_size);
+    fread(buf, input_size, 1, input);
+    
+    t_lexstate state;
+    lex_init(&state, (char *)buf);
+    lex_next_token(&state);
+    t_ast_node *code = parse_stmts(&state);
+    check_errors();
+    ast_node_print_lisp(code, 0);
+    
+    return 0;
 }
