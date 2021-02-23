af8cf12d3ca0e4916ff7a5f0dd4764ad22d594e1
bumbread <yyakut.ac@gmail.com>
2021-02-23 15:47:59 +1100

    fixed function empty param list and LISP formatting

--- src/main.c
+++ src/main.c
@@ -52,7 +52,7 @@ int main(void) {
     check_errors();
 #endif
     
-    t_ast_node *code = parse_ast_node_expr_level("a()");
+    t_ast_node *code = parse_ast_node_expr_level("a(a,b,2+2*3(2,3))");
     printf("output tree:\n\n");
     ast_node_print_lisp(code, 0);
     printf("\n");
